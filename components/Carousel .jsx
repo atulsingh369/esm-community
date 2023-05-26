d444e635f486ca209b3fb6a55f78fd3655f49329
atulsingh369 <businessatul7684@gmail.com>
2023-05-26 21:51:59 +0530

    Updated Form

--- components/Carousel .jsx
+++ components/Carousel .jsx
@@ -14,6 +14,8 @@ import { auth, db } from "../src/app/config";
 import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "../src/store";
 import { doc, setDoc } from "firebase/firestore";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Carousel = () => {
   const [state, setState] = useState(false);
@@ -30,6 +32,94 @@ const Carousel = () => {
   });
 
   const user = useSelector((state) => state.user);
+
+  const signUp = async () => {
+    setLoading(true);
+    if (!curUser.email || !curUser.password) {
+      toast.error("Enter Required Details");
+      setCurUser({
+        name: "",
+        email: "",
+        password: "",
+      });
+
+      setLoading(false);
+      setPasswordType("password");
+      return;
+    }
+    try {
+      // await sendSignInLinkToEmail(auth, curUser.email, actionCodeSettings);
+      const credential = await createUserWithEmailAndPassword(
+        auth,
+        curUser.email,
+        curUser.password
+      );
+      const res = credential.user;
+      await updateProfile(res, {
+        displayName: curUser.name,
+      });
+      await sendEmailVerification(res);
+      await setDoc(doc(db, "users", res.email), {
+        uid: res.uid,
+        displayName: res.displayName,
+        photoURL: res.photoURL,
+        email: res.email,
+      });
+      setState(!state);
+      setCurUser({
+        name: "",
+        email: "",
+        password: "",
+      });
+      const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+        if (currentUser && currentUser.uid === user.uid) {
+          currentUser.reload();
+          if (currentUser.emailVerified) {
+            // Email is verified, do nothing
+            unsubscribe(); // Stop listening for changes
+          } else {
+            // Email is not verified, delete the user
+            deleteDoc(doc(db, "users", user.email));
+            currentUser.delete();
+            toast.error("Email not verified, account deleted.");
+            unsubscribe(); // Stop listening for changes
+          }
+        }
+      });
+      toast.success("Registerd Succesfully");
+    } catch (error) {
+      toast.error(error.code);
+      console.log(error);
+      setCurUser({
+        name: "",
+        email: "",
+        password: "",
+      });
+    }
+  };
+
+  const signIn = async () => {
+    setLoading(true);
+    await signInWithEmailAndPassword(auth, curUser.email, curUser.password)
+      .then((userCredential) => {
+        const res = userCredential.user;
+        dispatch(setUser(res));
+        toast.success("Logined sucessfuly");
+        setCurUser({
+          email: "",
+          password: "",
+        });
+      })
+      .catch((error) => {
+        toast.error(error.code);
+        setCurUser({
+          email: "",
+          password: "",
+        });
+        setLoading(false);
+        setPasswordType("password");
+      });
+  };
 
   const googleLogin = async () => {
     await signInWithPopup(auth, provider)
@@ -62,6 +152,8 @@ const Carousel = () => {
         });
       });
   };
+
+  const changeState = () => setState(!state);
 
   return (
     <>
@@ -128,8 +220,34 @@ const Carousel = () => {
         </div>
         {!user && (
           <div className="w-full border-4 border-[#FF671F] border-dashed rounded-lg flex flex-col gap-2 justify-center items-center p-5">
-            <form className="form w-full rounded-md h-96">
-              <p id="heading">Login</p>
+            <div className="form w-full rounded-md h-96">
+              <p id="heading">{state ? "Login" : "SignUp"}</p>
+              {!state && (
+                <div className="field">
+                  <svg
+                    className="input-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    viewBox="0 0 16 16">
+                    <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
+                  </svg>
+                  <input
+                    autoComplete="off"
+                    required
+                    placeholder="Name"
+                    className="input-field"
+                    type="text"
+                    name="name"
+                    value={curUser.name}
+                    onChange={(e) =>
+                      setCurUser({ ...curUser, name: e.target.value })
+                    }
+                  />
+                </div>
+              )}
+
               <div className="field">
                 <svg
                   className="input-icon"
@@ -142,9 +260,15 @@ const Carousel = () => {
                 </svg>
                 <input
                   autoComplete="off"
+                  required
                   placeholder="Username"
                   className="input-field"
-                  type="text"
+                  type="email"
+                  name="email"
+                  value={curUser.email}
+                  onChange={(e) =>
+                    setCurUser({ ...curUser, email: e.target.value })
+                  }
                 />
               </div>
               <div className="field">
@@ -162,19 +286,59 @@ const Carousel = () => {
                   placeholder="Password"
                   className="input-field"
                   type="password"
+                  name="password"
+                  value={curUser.password}
+                  onChange={(e) =>
+                    setCurUser({ ...curUser, password: e.target.value })
+                  }
+                  required
                 />
               </div>
-              <div className="btn">
-                <button className="button1">
+              {/* <div className="btn">
+                <button onClick={signIn} className="button1">
                   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Login&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 </button>
-                <button className="button2">Sign Up</button>
-              </div>
+                <button onClick={signUp} className="button2">
+                  Sign Up
+                </button>
+              </div> */}
+              {state ? (
+                <button onClick={signIn} className="button1">
+                  Login
+                </button>
+              ) : (
+                <button onClick={signUp} className="button2">
+                  SignUp
+                </button>
+              )}
+
               <button className="button3">Forgot Password</button>
-            </form>
+              {state ? (
+                <p>
+                  Not Registered Yet ?
+                  <span
+                    className="text-primary cursor-pointer"
+                    onClick={changeState}>
+                    Register
+                  </span>
+                  here..
+                </p>
+              ) : (
+                <p>
+                  Already Registered ?
+                  <span
+                    className="text-primary cursor-pointer"
+                    onClick={changeState}>
+                    Login
+                  </span>
+                  here..
+                </p>
+              )}
+            </div>
             <GoogleButton onClick={googleLogin} />
           </div>
         )}
+        <ToastContainer />
       </div>
     </>
   );
